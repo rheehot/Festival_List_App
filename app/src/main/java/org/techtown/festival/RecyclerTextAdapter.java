82b253c4d7833fec47438548e909b21053b02ae1
soosungp33 <soosungp33@gmail.com>
2020-12-12 18:43:35 +0900

    로그인, 메인, 정보 액티비티 데이터 베이스 불러는 기능 구현 최종 완료

--- app/src/main/java/org/techtown/festival/RecyclerTextAdapter.java
+++ app/src/main/java/org/techtown/festival/RecyclerTextAdapter.java
@@ -1,16 +1,12 @@
 package org.techtown.festival;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
